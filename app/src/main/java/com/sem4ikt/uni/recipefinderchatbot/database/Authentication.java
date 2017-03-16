f5cbc0728598958483b1e2f45cc26fd454529d24
Mathias Lykke Pedersen <none>
2017-03-16 12:52:16 +0100

    Small change

--- app/src/main/java/com/sem4ikt/uni/recipefinderchatbot/database/Authentication.java
+++ app/src/main/java/com/sem4ikt/uni/recipefinderchatbot/database/Authentication.java
@@ -1,6 +1,12 @@
 package com.sem4ikt.uni.recipefinderchatbot.database;
 
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.sem4ikt.uni.recipefinderchatbot.LoginActivity;
 
 /**
  * Created by anton on 16-03-2017.
@@ -9,15 +15,26 @@ import com.google.firebase.auth.FirebaseAuth;
 public class Authentication implements IFirebaseAuth {
 
     private FirebaseAuth auth;
+    private LoginActivity loginActivity;
 
-    Authentication(){
+    Authentication(LoginActivity loginActivity){
+
+        this.loginActivity = loginActivity;
+
         auth = FirebaseAuth.getInstance();
     }
 
     @Override
     public void createUserWithEmailAndPassword(String email, String password) {
 
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(loginActivity, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
 
+
+                    }
+                });
 
     }
 
