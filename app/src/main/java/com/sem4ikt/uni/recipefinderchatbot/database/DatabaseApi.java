1be3c444baec4a2acd66fdc2c753562b36c5534e
Anton <antonzxcz@yahoo.dk>
2017-03-30 15:23:34 +0200

    starting on database insert and remove

--- app/src/main/java/com/sem4ikt/uni/recipefinderchatbot/database/DatabaseApi.java
+++ app/src/main/java/com/sem4ikt/uni/recipefinderchatbot/database/DatabaseApi.java
@@ -4,12 +4,17 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class DatabaseApi implements IDatabaseApi {
-   public boolean SaveRecipe(){
+    @Override
+    public boolean saveUser() {
+        return false;
+    }
+
+    public boolean SaveRecipe(){
        User user = new User("Anton","mail");
        DatabaseReference mDatabase;
 
        mDatabase = FirebaseDatabase.getInstance().getReference();
-       mDatabase.child("user").child("2").setValue(user);
+       mDatabase.child("user").child("2").push();
 
        return true;
     }
@@ -19,6 +24,12 @@ public class DatabaseApi implements IDatabaseApi {
     public boolean SaveCalories(){
         return true;
     }
+
+    @Override
+    public boolean getUser() {
+        return false;
+    }
+
     public boolean GetRecipe(){
         return true;
     }
