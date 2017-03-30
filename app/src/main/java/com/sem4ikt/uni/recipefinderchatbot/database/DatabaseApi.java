e404a9c0a38d11c76dafd890d76906c40877fd23
Kasper <kasperfriangt@gmail.com>
2017-03-30 16:54:25 +0200

    lavet getUser og SaveUser

--- app/src/main/java/com/sem4ikt/uni/recipefinderchatbot/database/DatabaseApi.java
+++ app/src/main/java/com/sem4ikt/uni/recipefinderchatbot/database/DatabaseApi.java
@@ -1,15 +1,23 @@
 package com.sem4ikt.uni.recipefinderchatbot.database;
 
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.sem4ikt.uni.recipefinderchatbot.model.spoonacular.MealPlanModel;
 
 public class DatabaseApi implements IDatabaseApi {
-    @Override
-    public boolean saveUser() {
-        return false;
-    }
 
-    public boolean SaveRecipe(){
+  public  boolean SaveUser(String userName, String UID){
+      DatabaseReference ref=FirebaseDatabase.getInstance().getReference("user");
+      ref.child(UID).setValue(userName);
+      return true;
+  }
+
+   public boolean SaveRecipe(){
        User user = new User("Anton","mail");
        DatabaseReference mDatabase;
 
@@ -18,16 +26,33 @@ public class DatabaseApi implements IDatabaseApi {
 
        return true;
     }
-    public boolean SaveMealplan(){
+    public boolean SaveMealplan(MealPlanModel mealPlan,String UID){
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference();
+        ref.child("Mealplan").child(UID).setValue(mealPlan);
+
         return true;
     }
     public boolean SaveCalories(){
         return true;
     }
-
+    private String userGetVar;
     @Override
-    public boolean getUser() {
-        return false;
+    public String getUser(String UID) {
+        DatabaseReference ref=FirebaseDatabase.getInstance().getReference("user");
+
+        ref.addListenerForSingleValueEvent(new ValueEventListener(){
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                userGetVar = (String) dataSnapshot.getValue();
+
+            }
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+        return userGetVar;
     }
 
     public boolean GetRecipe(){
--- app/src/main/java/com/sem4ikt/uni/recipefinderchatbot/database/IDatabaseApi.java
+++ app/src/main/java/com/sem4ikt/uni/recipefinderchatbot/database/IDatabaseApi.java
@@ -1,15 +1,17 @@
 package com.sem4ikt.uni.recipefinderchatbot.database;
 
+import com.sem4ikt.uni.recipefinderchatbot.model.spoonacular.MealPlanModel;
+
 /**
  * Created by kaspe on 30-03-2017.
  */
 
 public interface IDatabaseApi {
-    boolean saveUser();
+    boolean SaveUser(String userName, String UID);
     boolean SaveRecipe();
-    boolean SaveMealplan();
+    boolean SaveMealplan(MealPlanModel mealPlan,String UID);
     boolean SaveCalories();
-    boolean getUser();
+    String getUser(String UID);
     boolean GetRecipe();
     boolean GetMealplan();
     boolean GetCalories();
