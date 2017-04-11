61770bb4e44f47b2ffcc85cde151a87a98267a78
henrikniesen <201507091@post.au.dk>
2017-04-11 12:06:57 +0200

    Added createRecipeListContainer to ListContainerFactory

--- app/src/main/java/com/sem4ikt/uni/recipefinderchatbot/adapter/ListContainerFactory.java
+++ app/src/main/java/com/sem4ikt/uni/recipefinderchatbot/adapter/ListContainerFactory.java
@@ -3,6 +3,7 @@ package com.sem4ikt.uni.recipefinderchatbot.adapter;
 import com.sem4ikt.uni.recipefinderchatbot.model.interfaces.ListDataModel;
 import com.sem4ikt.uni.recipefinderchatbot.model.spoonacular.IngredientsModel;
 import com.sem4ikt.uni.recipefinderchatbot.model.spoonacular.NutrientsModel;
+import com.sem4ikt.uni.recipefinderchatbot.model.spoonacular.RecipeModel;
 import com.sem4ikt.uni.recipefinderchatbot.model.spoonacular.RecipesModel;
 
 import java.util.ArrayList;
@@ -32,5 +33,21 @@ public class ListContainerFactory {
         List<Object> list = new ArrayList<Object>(source);
 
         return new ListDataContainer(list, ListDataModel.ListDataType.INGREDIENT);
+    }
+
+    public static ListDataContainer createRecipeListContainer(List<RecipeModel> source) {
+
+        List<Object> list = new ArrayList<Object>();
+
+        for (RecipeModel model : source) {
+            RecipesModel tmpmodel = new RecipesModel();
+            tmpmodel.setId(model.getId());
+            tmpmodel.setTitle(model.getTitle());
+            tmpmodel.setImage(model.getImage());
+            tmpmodel.setReadyInMinutes(model.getReadyInMinutes());
+            list.add(tmpmodel);
+        }
+
+        return new ListDataContainer(list, ListDataModel.ListDataType.RECIPE);
     }
 }
