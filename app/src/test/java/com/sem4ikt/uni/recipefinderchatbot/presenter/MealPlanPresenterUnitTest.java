fed61bb92dc1c87261e349f5a71f19d6cf682da5
Kasper <kasperfriangt@gmail.com>
2017-04-27 16:09:12 +0200

    mere refactorering på mealplan

--- app/src/test/java/com/sem4ikt/uni/recipefinderchatbot/presenter/MealPlanPresenterUnitTest.java
+++ app/src/test/java/com/sem4ikt/uni/recipefinderchatbot/presenter/MealPlanPresenterUnitTest.java
@@ -1,5 +1,8 @@
 package com.sem4ikt.uni.recipefinderchatbot.presenter;
 
+import android.widget.ScrollView;
+import android.widget.TextView;
+
 import com.sem4ikt.uni.recipefinderchatbot.database.Interface.ICallbackMealplan;
 import com.sem4ikt.uni.recipefinderchatbot.database.Interface.IFirebaseDBInteractors;
 import com.sem4ikt.uni.recipefinderchatbot.model.spoonacular.MealPlanDayModel;
@@ -36,7 +39,7 @@ public class MealPlanPresenterUnitTest {
     @Before
     public void setup() throws Exception {
 
-        presenter = new MealPlanPresenter(mealPlanView,interactor);
+        presenter = new MealPlanPresenter(mealPlanView, interactor);
     }
 
     @Test
@@ -55,55 +58,54 @@ public class MealPlanPresenterUnitTest {
     }
 
     @Test
-    public void getMealplanDayTest(){
+    public void getMealplanDayTest() {
         presenter.getMealPlanDay();
-        verify(interactor,times(1)).getMealPlanDay(presenter);
+        verify(interactor, times(1)).getMealPlanDay(presenter);
     }
 
     @Test
-    public void getMealplanWeekTest(){
+    public void getMealplanWeekTest() {
         presenter.getMealPlanWeek();
-        verify(interactor,times(1)).getMealPlanWeek(presenter);
+        verify(interactor, times(1)).getMealPlanWeek(presenter);
     }
 
     @Test
-    public void setDateToTwelveTest(){
-        Date dato= new Date();
-        Calendar cal=Calendar.getInstance();
-        cal.set(Calendar.HOUR_OF_DAY,12);
-        cal.set(Calendar.SECOND,0);
-        cal.set(Calendar.MINUTE,0);
-        cal.set(Calendar.MILLISECOND,0);
-        Assert.assertEquals(presenter.setDateToTwelve(dato).getTime(),cal.getTime().getTime());
+    public void setDateToTwelveTest() {
+        Date dato = new Date();
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY, 12);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        Assert.assertEquals(presenter.setDateToTwelve(dato).getTime(), cal.getTime().getTime());
     }
 
 
-    public void loadMealplansTestWithNonValidInfo(){
+    public void loadMealplansTestWithNonValidInfo() {
         Assert.assertNull(presenter.loadMealplans(new Date()));
     }
 
     @Test
-    public  void onReceivedValidMealPlanWeek(){
-        presenter.onReceivedWeek(new ArrayList<MealPlanWeekModel>(),new ArrayList<Date>(), ICallbackMealplan.MEALPLAN_CALLBACK_TYPE.GET_MEALPLAN_WEEK);
-        verify(mealPlanView,times(1)).getWeekPlan(new ArrayList<MealPlanWeekModel>(),new ArrayList<Date>());
+    public void onReceivedValidMealPlanWeek() {
+        presenter.onReceivedWeek(new ArrayList<MealPlanWeekModel>(), new ArrayList<Date>(), ICallbackMealplan.MEALPLAN_CALLBACK_TYPE.GET_MEALPLAN_WEEK);
+        verify(mealPlanView, times(1)).getWeekPlan(new ArrayList<MealPlanWeekModel>(), new ArrayList<Date>());
     }
 
     @Test
-    public  void onReceivedValidMealPlanDay(){
-        presenter.onReceivedDay(new ArrayList<MealPlanDayModel>(),new ArrayList<Date>(), ICallbackMealplan.MEALPLAN_CALLBACK_TYPE.GET_MEALPLAN_DAY);
-        verify(mealPlanView,times(1)).getDayPlan(new ArrayList<MealPlanDayModel>(),new ArrayList<Date>());
+    public void onReceivedValidMealPlanDay() {
+        presenter.onReceivedDay(new ArrayList<MealPlanDayModel>(), new ArrayList<Date>(), ICallbackMealplan.MEALPLAN_CALLBACK_TYPE.GET_MEALPLAN_DAY);
+        verify(mealPlanView, times(1)).getDayPlan(new ArrayList<MealPlanDayModel>(), new ArrayList<Date>());
     }
 
     @Test
-    public  void onReceivedNothingMealPlanWeek(){
-        presenter.onReceivedWeek(null,null, ICallbackMealplan.MEALPLAN_CALLBACK_TYPE.GET_MEALPLAN_WEEK);
-        verify(mealPlanView,times(0)).getWeekPlan(null,null);
+    public void onReceivedNothingMealPlanWeek() {
+        presenter.onReceivedWeek(null, null, ICallbackMealplan.MEALPLAN_CALLBACK_TYPE.GET_MEALPLAN_WEEK);
+        verify(mealPlanView, times(0)).getWeekPlan(null, null);
     }
 
     @Test
-    public  void onReceivedNothingMealPlanDay(){
-        presenter.onReceivedDay(null,null, ICallbackMealplan.MEALPLAN_CALLBACK_TYPE.GET_MEALPLAN_DAY);
-        verify(mealPlanView,times(0)).getDayPlan(null,null);
+    public void onReceivedNothingMealPlanDay() {
+        presenter.onReceivedDay(null, null, ICallbackMealplan.MEALPLAN_CALLBACK_TYPE.GET_MEALPLAN_DAY);
+        verify(mealPlanView, times(0)).getDayPlan(null, null);
     }
-
 }
